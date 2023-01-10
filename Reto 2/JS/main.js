8aa006a3e5ce6d8d7096ea648d476f50a8707c84
Eduard <eduardsanguinocastro@gmail.com>
2023-01-10 11:43:12 +0100

    Se ha realizado y comprobado el reto nº 2 y se ha actualizado el README.md

--- Reto 2/JS/main.js
+++ Reto 2/JS/main.js
@@ -1,35 +1,25 @@
-// Establecemos la constante contraseña.
-var pass = 'Hol@334';
-//Añadimos la variable intento que será la que irá marcando al programa cuantos intentos lleva el usuario.
-var intento = 0;
-//Cómo extra añadimos una variable que recoja el div "resultado" de dentro del html para poder mostrar el resultado del ejercicio también en la web.
-let divweb = document.getElementById('resultado');
-//Creamos la variable que almacenará la contraseña que escribirá el usuario en esta Prompt.
-//var input = prompt('Introduce la contraseña');
-//Tras el intento del usuario de acertar la contraseña insertamos la orden que le sumará 1 a la variable "intento".
-intento++;
-//Como queremos que el usuario sólo tenga 3 intentos creamos la variable "oportunidades" que llevará la cuenta de los intentos restantes. 
-var oportunidades = 3 - intento;
-//Creamos un bucle que mientras el usuario haya echo menos de 3 intentos y además la contraseña puesta por el usuario no coincida con la variable "pass" mandara una alerta de que la contraseña es incorrecta mostrando los intentos restantes, te volverá a pedir la contraseña y aumentara el contador de "intento" en uno.
-while (intento < 3 && pass != input){
-    alert('¡Contraseña incorrecta! intentos restantes: ' + oportunidades)
-    oportunidades= oportunidades - intento;
-    //var input = prompt('Intentelo de nuevo');
-    intento++;    
+//Creamos la función que creará los números aleatorios.
+
+function random (minimo, maximo){
+    minimo = Math.ceil (min);
+    maximo = Math.floor (max + 1);
+    return Math.floor(Math.random() * (maximo - minimo) + minimo);
 }
 
-//Creamos una condición if para saber si el bucle while se ha finalizado por exceder los intentos o porque el usuario ha acertado la contraseña, para ello comparamos que el usuario haya echo 3 o menos intentos y que la contraseña se ha acertado, si se cumple querrá decir que el usuario ha acertado la contraseña, y se mostrará por consola y por pantalla
-if (intento <= 3 && input == pass){
-    console.log ('¡Enhorabuena! ¡Contraseña correcta!');
-    divweb.innerHTML = '¡Enhorabuena! ¡Contraseña correcta!';
-}// En el caso de que no se cumpla quiere decir que el bucle while finalizó porque el usuario se equivocó 3 veces al poner la contraseña y se mostrará en pantalla y consola que se le han acabado los intentos
-else{
-    console.log ('Ha excedido los intentos, acceso denegado.');
-    divweb.innerHTML = 'Ha excedido los intentos, acceso denegado.';
-    
-};
-    
+// Creamos la variable que nos vincula con el div del html.
+let divweb = document.getElementById('resultado');
+// Pedimos al usuario entre que dos números quiere que se creen los aleatorios y cuántos aleatorios quiere que se creen.
+var min = Number(prompt("Introduce desde que número quieres que se cree el aleatorio"));
+var max = Number(prompt("Introduce hasta que número quieres que se cree el aleatorio"));
+var numbers = Number(prompt ("¿Cuántos números aleatorios quieres que se creen?"))
+//Crearemos una array vacía dónde insertaremos los números aleatorios que se irán creando en el bucle for para luego poder mostrarlos de una manera mas ordenada.
+var aleatorios = []
 
-
-
-//Mandamos el cálculo mátematico al div de la web.
+//Creamos un bucle for que irá creando tantos números aleatorios como hayamos pedido y los irá añadiendo al array vacío creado antes.
+for (i = 0; i < numbers; i++){
+    aleatorio = random(min, max)
+    aleatorios.push(aleatorio);
+}
+//Una vez generados los números aleatorios sólo hace falta mostrarlos imprimiendo el array e indicando cómo queremos que se separen éstos.
+console.log(aleatorios.join(", "));
+divweb.innerHTML = aleatorios.join(", ");
